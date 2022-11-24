bd470e6d8e5246e9e59d65957339a0d1b212eacc
Leafboi <110484176+Leafboi111@users.noreply.github.com>
2022-11-24 11:18:43 -0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,8 @@
-echo "Hello, World!"
+clear
+
+tput setaf 1
+echo "WARNING: This is an EXPERIMENTAL feature. Use at your own risk."
+
+read -p "Repo you want to add: " repo
+
+echo "$repo" >> data/applist.txt
